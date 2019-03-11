a67ca94d753757eee424dfc13ad6799a549633ae
sadiktlan <talansadik@gmail.com>
2019-03-11 21:56:18 +0300

    adding components

---
+++ src/Components/MenuItems/SidebarItem.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SidebarItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default SidebarItem;
